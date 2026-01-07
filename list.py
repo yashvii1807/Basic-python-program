f465d38831e6d47af46877f3d55acd2cdd6e37ed
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-01-07 18:18:50 +0530

    list

---
+++ list.py
@@ -0,0 +1,40 @@
+# list in python 
+
+# a build-in data types that stores set of values. it can stores elemnt of different types(int,float,string,etc)
+
+# marks = [45,465,78,684,848,74,84,1,59]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+
+
+# student = ["yashvi" , 840.5 , "mendarda"]
+# student[0] = "vaibhav"
+# print((student))
+
+
+#list slicing
+#similar to string slicing
+
+
+# marks = [74,56,84,53,48,85]
+# print(marks[:2])
+
+
+#list methods
+# list = [2,1,3]
+# list.append(4) #adds one element at the end [2,1,3,4]
+# print(list)
+# list.sort() #sorts in ascending order[1,2,3]
+# print(list)
+# list.sort(reverse=True) #sorts in descending order[3,2,1]
+# print(list)
+# list.reverse() #reverses list[3,1,2]
+# print(list)
+# list.insert(2,10) #insert element at index
+# print(list)
+#list.remove(1) #remove first occurrence of elemnt
+#print(list)
+#list.pop(idx) #removes elemnt at idx
+#print(list)
