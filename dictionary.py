2aa252b8cc704fcc3d93a6b657a2e0cc19ffbc84
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-01-12 19:09:11 +0530

    updated dictionary

--- dictionary.py
+++ dictionary.py
@@ -17,5 +17,60 @@
 # info["surname"] = "chauhan"
 # print(info)   
 
-#nested directorey
+#nested directory
+students = {
+    "name" : "yashuu",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+
+# print((students["subjects"]))
+# students.update({"age" : 20})
+# print(students)
+new_dict = {"city" : "rajkot"}
+students.update(new_dict)
+print(students)
+
+
+
+# #dictionary methods
+# print(len(list(students.keys())))
+# print(list(students.values()))
+
+# pairs = list(students.items())
+# print(pairs[0])
+
+# print(students["name"]) # 
+# print(students.get("name"))
+
+# myDict.keys()  # returns all keys
+# myDict.values() #return all values
+# myDict.items() # return all (key , val) pair as tuples
+# myDict.get("key") #return the key according to value
+# myDict.update(newDict) #insert the specification to the dictionary
+
+
+
+
+#practice que
+#1 : store following word meaning in python dictionary
+#table : "a pice of furnitre", "list of facts & figures"
+#cat : "a small animal"
+
+# yashvi = {
+#     "table" :
+#     ["a piece of furmiture , list of facts & figures"],
+#     "cat" : { 
+#         "cat": "a samll animal"
+#     }
+# }
+# print(yashvi["table"])
+
+# print(yashvi["cat"]["cat"])
+
+
+#2 : 
 
---
+++ set.py
@@ -0,0 +1,46 @@
+#set is the collection of the unordered items. each element in the set must  be unique & immutable.
+
+collection = {1 ,2,3,2,3,4,"hello","world","hello"}
+print(collection)
+print(type(collection))
+print(len(collection))
+
+
+# collection = set()
+# print(collection)
+
+#set methods
+# set.add(el) # adds an element
+# set.remove(el) #removes the element
+# set.clear() #empties the set 
+# set.pop() #removes a radom value
+#set.union(set2) combines both set values & return new
+#set.intersection(set2) combines common values & return new 
+
+
+
+# collection = set()
+# collection.add(1)
+# collection.add(2)
+# collection.add(3)
+
+# collection.clear()
+# # collection.remove(1)
+# print(len(collection))
+
+#collection = {"hello", "yashvi", "how are you", "vaibhav"}
+#print(collection.pop())
+
+# set1 = {1,2,3}
+# set2 = {2,3,4}
+
+# print(set1.union(set2))
+# print(set1)
+# print(set2)
+
+
+# set1 = {1,2,3}
+# set2 = {2,3,4}
+# print(set1.intersection(set2))
+
+
