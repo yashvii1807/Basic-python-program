a9381b16b6a357af9241eb2f5da004e12a6b27b8
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-06-26 16:22:22 +0530

    updated code

--- oops.py
+++ oops.py
@@ -282,12 +282,29 @@
 
 
 #property decorator: we use @property decorator on any method in the class to use the method as a property.
-class Student:
-    def __init__(self, phy , chem, math):
-        self.phy = phy
-        self.chem = chem
-        self.math = math
-        self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
-stu1 = Student(90, 97, 99)
-print(stu1.percentage)        
+# class Student:
+#     def __init__(self, phy , chem, math):
+#         self.phy = phy
+#         self.chem = chem
+#         self.math = math
+#     #     self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
+
+#     # def calcupercentage(self):
+#     #     self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
+
+
+#     @property
+#     def percentage(self):
+#         return str((self.phy + self.chem + self.math) / 3) + "%"
+
+
+# stu1 = Student(90, 97, 99)
+# print(stu1.percentage)        
+
+# # stu1.phy = 54
+# # print(stu1.phy)
+# # stu1.calcupercentage()
+# print(stu1.percentage)
+
+
 
