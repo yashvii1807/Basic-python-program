a33878ec940c1013128fc75e51db8536d38299aa
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-07-06 15:48:24 +0530

    advance python

---
+++ iterators.py
@@ -0,0 +1,3 @@
+#iterators: iterators are objects that allow you to traverse through a collection of elements, such as lists, tuples, or dictionaries. In Python, an iterator is an object that implements the iterator protocol, which consists of two methods: __iter__() and __next__().
+#iterators are advanced python concepts that allow for efficient looping and memory management. Iterators provide a way to access elements of collection sequentially without exposing the underlying sturcture.
+
