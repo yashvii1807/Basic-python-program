a9c3b918d2bc09541e66ee5b298e12894fe3b15e
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-07-07 15:05:33 +0530

    pattern

--- pattern.py
+++ pattern.py
@@ -40,11 +40,27 @@
 # *    
 
 
-n = 4
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        if i == 1 or i == n or j == 1 or j == n:
-            print("*", end=" ")
+# n = 4
+# for i in range(1, n+1):
+#     for j in range(1, n+1):
+#         if i == 1 or i == n or j == 1 or j == n:
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")
+#     print() 
+
+
+
+
+#heart pattern
+
+for i in range(6):
+    for j in range (7):
+        if (i == 0 and j % 3 != 0) or \
+        (i == 1 and j % 3 == 0) or \
+        (i - j == 2) or \
+        (i + j == 8):
+          print("*", end="")
         else:
-            print(" ", end=" ")
-    print() 
+            print(" ", end="")
+    print()              
