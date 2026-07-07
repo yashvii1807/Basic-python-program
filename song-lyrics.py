655ea04e6d5f76019b4150657bcad45f74a52560
yashvi sidapara <sidaparayashvi18@gmail.com>
2026-07-07 15:33:29 +0530

    song-lyrics

---
+++ song-lyrics.py
@@ -0,0 +1,27 @@
+import time
+import sys
+
+def print_line(line, char_delay=0.065):
+    for char in line:
+        print(char, end='', flush=True)
+        time.sleep(char_delay)
+    print()  
+
+def print_lyrics():
+    lyrics = [
+        "Haathon ko sambhaale mere haathon mein",
+        "kaise haathon ko sambhaale mere haathon mein",
+        "jab tak neend na aaye in lakeeron mein",
+        "baatein hon...",
+        "haan..."
+    ]
+    delays = [2.0, 1.8, 2.1, 2.4, 1.7] 
+
+    print("Arz kiya hai lyrics:\n")
+    time.sleep(1.5)
+
+    for i, line in enumerate(lyrics):
+        print_line(line)
+        time.sleep(delays[i]) 
+
+print_lyrics()                
